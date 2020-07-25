1dfd8371bb4169be6e00f06b1d350606b34b6f6f
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2020-07-25 17:36:32 +0100

    register route updated

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("this is register home");
+});
 
-router.get('/', (req, res) => {
-  res.send('this is register home')
-})
+router.get("/users/register", (req, res) => {
+  res.render("register");
+});
 
-router.get('/users/register', (req, res) => {
-res.render('register')
-})
+router.get("/users/login", (req, res) => {
+  res.render("login");
+});
 
+router.post("/users/register", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const password2 = req.body.password2;
 
-router.get('/users/login', (req, res) => {
- res.render('login')
-})
-
-
-
-
+  if(name != '' || email != '' || password != '' | password2 != ''){
+    res.render('login')
+  }
+});
 
 module.exports = router;
