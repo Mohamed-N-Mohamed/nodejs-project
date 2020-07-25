733d0b2e64e79fcb3a6b92bdd74c5c924eae865c
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2020-07-25 17:30:37 +0100

    added connection to mongodb

--- app.js
+++ app.js
@@ -5,17 +5,32 @@ const app = express();
 //router
 const router = require("./routes/index");
 
+//mongoose
+const mongoose = require("mongoose");
+
 const expressLayouts = require("express-ejs-layouts");
 
 app.use(expressLayouts);
 
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', router)
+//db uri
+const db = 'mongodb+srv://deku123:deku123@cluster0.knuyw.mongodb.net/Users?retryWrites=true&w=majority";'
+
+// connect to mongoose
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log(err));
+
+app.use("/", router);
 
 //port server
 const port = 3000;
 
-app.listen(port, console.log('server is running on 3000'));
+app.listen(port, console.log("server is running on 3000"));
